e8d7a324a0fe0e6b250b64ee98de7b902177c579
MascaraSnake <jonassauer27@gmail.com>
2016-01-05 14:31:30 +0100

    Added "All files" filter for open/save map dialogs (not for choosing the IWAD though, because that's always .wad or .srb)

--- Source/Core/General/General.cs
+++ Source/Core/General/General.cs
@@ -1204,7 +1204,7 @@ namespace CodeImp.DoomBuilder
 
 			// Open map file dialog
 			OpenFileDialog openfile = new OpenFileDialog();
-			openfile.Filter = "Doom WAD Files (*.wad)|*.wad";
+			openfile.Filter = "WAD Files (*.wad)|*.wad|All files|*.*";
 			openfile.Title = "Open Map";
 			if(!string.IsNullOrEmpty(settings.LastUsedMapFolder) && Directory.Exists(settings.LastUsedMapFolder)) //mxd
 				openfile.InitialDirectory = settings.LastUsedMapFolder; //mxd
@@ -1501,7 +1501,7 @@ namespace CodeImp.DoomBuilder
 
 			// Show save as dialog
 			SaveFileDialog savefile = new SaveFileDialog();
-			savefile.Filter = "Doom WAD Files (*.wad)|*.wad";
+			savefile.Filter = "WAD Files (*.wad)|*.wad|All files|*.*";
 			savefile.Title = "Save Map As";
 			savefile.AddExtension = true;
 			savefile.CheckPathExists = true;
@@ -1573,7 +1573,7 @@ namespace CodeImp.DoomBuilder
 
 			// Show save as dialog
 			SaveFileDialog savefile = new SaveFileDialog();
-			savefile.Filter = "Doom WAD Files (*.wad)|*.wad";
+			savefile.Filter = "WAD Files (*.wad)|*.wad|All files|*.*";
 			savefile.Title = "Save Map Into";
 			savefile.AddExtension = true;
 			savefile.CheckPathExists = true;
